842e2a6382ceffc96cd7e9c5732d1591983cdca7
Marina Cerame <marina.cerame@gmail.com>
2016-11-30 17:06:00 -0600

    song model passes

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,15 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.models.length === 1) {
+        this.playFirst();
+      }
+    });
+  },
+
+  playFirst: function(param) {
+    //param.play();
   }
 
 });
