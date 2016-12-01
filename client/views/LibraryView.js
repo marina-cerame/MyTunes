ae482b51ab8f03d93a6d76dbb0bc774704d0ac6c
Marina Cerame <marina.cerame@gmail.com>
2016-12-01 15:56:42 -0600

    PASSIN THEM TESTS YO

--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -5,7 +5,7 @@ var LibraryView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
-
+    this.listenTo(this.collection, 'sync', this.render, this);
   },
 
   render: function() {
