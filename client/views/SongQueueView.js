fe5a54e14c8f55306e4e8d48a4162dda0eacb6cd
Marina Cerame <marina.cerame@gmail.com>
2016-12-01 17:27:58 -0600

    Queue actually shows up on the page

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,13 +1,24 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+  className: 'queue',
+  
   initialize: function() {
+    this.collection.on('add remove', this.render, this);
     this.render();
-    SongQueueEntryView.prototype.render();
+    // SongQueueEntryView.prototype.render();
   },
 
   render: function() {
-    return this.$el;
+    this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+    // return this.$el;
   }
 
 });
